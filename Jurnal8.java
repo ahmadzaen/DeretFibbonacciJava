185b6eee056a9f3ebacb6efe2e03d63f9ce37624
Ahmad Zaeni 6706174120 <43094463+ahmadzaen@users.noreply.github.com>
2019-04-06 11:14:45 +0700

    Update Jurnal8.java

--- Jurnal8.java
+++ Jurnal8.java
@@ -26,7 +26,7 @@ public class Jurnal8 {
         fib[1] = 1;
          
         for(int i = 2; i < n; i++) {
-            fib[i] = fib[i-1] + fib[i-2];
+            fib[i] = fib[i-3] + fib[i-1];
         }
          
         for (int i = 0; i < n; i++) {
